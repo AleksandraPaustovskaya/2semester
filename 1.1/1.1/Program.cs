be736e1b7951a81aac10e43a2143748fefe9cb71
Aleksandra_Paustovskaya <paustovskayasasha@gmail.com>
2021-03-23 18:12:54 +0300

    1.1 первая версия

---
+++ 1.1/1.1/Program.cs
@@ -0,0 +1,38 @@
+﻿//Посчитать факториал
+
+using System;
+
+namespace _1._1
+{
+    class Program
+    {
+        private static int FactorialCalculation(int factorial)
+        {
+            int answer = 1;
+            int maxValue = int.MaxValue;
+            for (int helper = 1; helper <= factorial; helper++)
+            {
+                if (maxValue/helper < answer)
+                {
+                    return 0;
+                }
+                answer *= helper;
+            }
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to calculate factorial");
+            int factorial = Convert.ToInt32(Console.ReadLine());
+            int answer = FactorialCalculation(factorial);
+            if (answer > 0)
+            {
+                Console.WriteLine("The factorial of " + factorial + " is " + FactorialCalculation(factorial));
+            }
+            else
+            {
+                Console.WriteLine("Over the limit number");
+            }
+        }
+    }
+}
